ccca4f61cef82962c72e73e98153533a71634b50
DaanKlaassen <daan.klaassen@student.gildeopleidingen.nl>
2024-12-03 11:14:41 +0100

    dice game commit

--- die.gd
+++ die.gd
@@ -1,5 +1,7 @@
 extends RigidBody3D
 
+@onready var raycasts = $Raycasts.get_children()
+
 
 var start_pos
 var roll_strength = 30
@@ -32,3 +34,10 @@ func _roll():
 	var throw_vector = Vector3(randf_range(-1, 1), 0, randf_range(-1, 1)).normalized()
 	angular_velocity = throw_vector * roll_strength / 2
 	apply_central_impulse(throw_vector * roll_strength)
+
+
+func _on_sleeping_state_changed():
+	if sleeping:
+		for raycast in raycasts:
+			if raycast.is.colliding():
+				roll_finished.emit(raycast.opposite_side)
